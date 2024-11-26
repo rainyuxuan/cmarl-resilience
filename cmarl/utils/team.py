9c4c335ad6c19118cfe8981d6e395e2066698f32
rainyuxuan <yu2001xuan@outlook.com>
2024-11-26 13:19:11 -0500

    Updated TeamManager

--- cmarl/utils/team.py
+++ cmarl/utils/team.py
@@ -1,16 +1,42 @@
 
-def group_agents(agents: list[str]) -> dict[str, list[str]]:
-    """
-    Group agents by their team.
-    :param agents: a list of agent names in the format of teamname_agentid
-    :return: a dictionary with team names as keys and a list of agent names as values
-    """
-    teams = {}
-    for agent in agents:
-        team, _ = agent.split('_')
-        if team in teams:
-            teams[team].append(agent)
-        else:
-            teams[team] = [agent]
-    return teams
+class TeamManager:
 
+    def __init__(self, agents: list[str]):
+        self.agents = agents
+        self.teams = self.group_agents()
+        self.terminated_agents = set()
+
+    def group_agents(self) -> dict[str, list[str]]:
+        """
+        Group agents by their team.
+        :param agents: a list of agent names in the format of teamname_agentid
+        :return: a dictionary with team names as keys and a list of agent names as values
+        """
+        teams = {}
+        for agent in self.agents:
+            team, _ = agent.split('_')
+            if team in teams:
+                teams[team].append(agent)
+            else:
+                teams[team] = [agent]
+        return teams
+
+    def reset(self):
+        self.terminated_agents = set()
+
+    def is_team_terminated(self, team: str):
+        """
+        Check if all agents in a team are terminated.
+        :param team: the team name
+        :return: True if all agents in the team are terminated, False otherwise
+        """
+        assert team in self.teams, f"Team [{team}] not found."
+        return all(agent in self.terminated_agents for agent in self.teams[team])
+
+    def terminate_agent(self, agent: str):
+        """
+        Mark an agent as terminated.
+        :param agent:
+        :return:
+        """
+        self.terminated_agents.add(agent)
